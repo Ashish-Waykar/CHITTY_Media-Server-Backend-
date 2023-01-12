03ca9e466ca591084d719947d5b786216288211a
Ashish-Waykar <ashishwaykar19@gmail.com>
2023-01-12 09:47:03 +0530

    signup model for user user registration has been done

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const router=express.Router()
+const mongoose = require('mongoose')
+const User = mongoose.model('User') 
 
 router.get('/',(req,res)=>{
      res.send("new route is done");
@@ -10,6 +12,22 @@ router.post('/signup',(req,res)=>{
     if(!email || !name || !pass){
         return res.status(422).json({error:"Fill complete form"});   
     }
-    res.json({message:"new route about is done"})
+    // res.json({message:"new route about is done"})
+    User.findOne({email:email})
+    .then((saveUser)=>{
+        if(saveUser){
+            return res.json({message:"User already exists"})
+        }
+        const user = new User({
+            email:email,password:pass,name:name
+        })
+        user.save()
+        .then(user=>{
+            res.json({message:"User Saved Successfully "})
+        }).catch(err=>{
+            console.log(err)
+        })
+
+    }).catch(err=>{console.log(err)})
 })
 module.exports = router
