f59710a1075981d57d3e737fa6345d6a302d3dfb
Ashish-Waykar <ashishwaykar19@gmail.com>
2023-01-12 10:02:17 +0530

    signup password has been encrypted

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router=express.Router()
 const mongoose = require('mongoose')
 const User = mongoose.model('User') 
-
+const bcrypt= require('bcryptjs')
 router.get('/',(req,res)=>{
      res.send("new route is done");
 })
@@ -18,16 +18,20 @@ router.post('/signup',(req,res)=>{
         if(saveUser){
             return res.json({message:"User already exists"})
         }
-        const user = new User({
-            email:email,password:pass,name:name
+        bcrypt.hash(pass,12) // bigger integer you pass perameter more secure password will be recived 
+        .then(hashedPassword=>{
+            const user = new User({
+                email:email,password:hashedPassword,name:name
+            })
+            user.save()
+            .then(user=>{
+                res.json({message:"User Saved Successfully "})
+            }).catch(err=>{
+                console.log(err)
+            })
+    
         })
-        user.save()
-        .then(user=>{
-            res.json({message:"User Saved Successfully "})
-        }).catch(err=>{
-            console.log(err)
-        })
-
+        
     }).catch(err=>{console.log(err)})
 })
 module.exports = router
