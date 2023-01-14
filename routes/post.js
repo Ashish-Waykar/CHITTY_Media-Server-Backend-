e51252f96cd40c5c93947bc2ae1b10b5694e577b
Ashish-Waykar <ashishwaykar19@gmail.com>
2023-01-14 12:11:53 +0530

    created my posts route completed

--- routes/post.js
+++ routes/post.js
@@ -37,4 +37,14 @@ router.post('/createpost',requirelogin,(req,res)=>{
         console.log(err);
     })
 })
+router.get('/mypost',requirelogin,(req,res)=>{
+    Post.find({postedBy:req.user._id})
+    .populate("postedBy","_id name")
+    .then(posts=>{
+        res.json({post:posts})
+    }).catch(err=>{
+        console.log(err)
+    })
+})
+
 module.exports= router 
